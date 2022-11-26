686df21d58cfb7a098426710c1bb8337427a5b02
dj-lei <m18349125880@gmail.com>
2022-11-26 10:53:43 +0800

    add files server

--- view.py
+++ view.py
@@ -14,7 +14,7 @@ def apply_caching(response):
 @app.route("/query_themes", methods=['GET'])
 def query_themes():
     if request.method == 'GET':
-        themes = iterate_files_in_directory(cf['ENV_'+env]['TEXT_LOGIC_CONFIG_STORE_PATH'])
+        themes = iterate_files_in_directory(cf['ENV_'+env]['TEXT_ANALYSIS_THEMES_STORE_PATH'])
         return jsonify({'status': 'ok', 'themes': themes})
     return jsonify({'status': 'error'})
 
@@ -23,7 +23,7 @@ def save_theme():
     if request.method == 'GET':
         filename = request.args.get('filename')
         theme = request.args.get('theme')
-        path = cf['ENV_'+env]['TEXT_LOGIC_CONFIG_STORE_PATH']+filename.split('\\')[-1]
+        path = cf['ENV_'+env]['TEXT_ANALYSIS_THEMES_STORE_PATH']+filename.split('\\')[-1]
         f = open(path, "w")
         f.write(theme)
         f.close()
@@ -34,8 +34,14 @@ def save_theme():
 def download_theme(filename):
     if request.method == 'GET':
         filename = request.view_args['filename']
-        return send_file(cf['ENV_'+env]['TEXT_LOGIC_CONFIG_STORE_PATH']+filename)
+        return send_file(cf['ENV_'+env]['TEXT_ANALYSIS_THEMES_STORE_PATH']+filename)
     return jsonify({'status': 'error'})
 
+# simple files server
+@app.route("/download/<filename>")
+def download(filename):
+    path = cf['ENV_'+env]['TEXT_ANALYSIS_THEMES_STORE_PATH']+filename
+    return send_file(path)
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8001)
+    app.run(host='0.0.0.0', port=8001)
